8b76368278b6c944d63923a3c5f7e3da2c6254fe
Michael Hall <michaelhall923@protonmail.com>
2025-10-22 14:19:39 -0400

    fix meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,42 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <meta
-          name="title"
-          content="Grove Marketing Co. | Digital Marketing, Web Development & SEO"
-        />
-        <meta
-          name="description"
-          content="Grove Marketing Co. helps bold brands grow with cutting-edge marketing, custom websites, content creation, and automation. Let’s make waves together."
-        />
-
-        {/* Open Graph */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.grovemarketingco.com/" />
-        <meta
-          property="og:title"
-          content="Grove Marketing Co. | Digital Marketing, Web Development & SEO"
-        />
-        <meta
-          property="og:description"
-          content="Grove Marketing Co. helps bold brands grow with cutting-edge marketing, custom websites, content creation, and automation. Let’s make waves together."
-        />
-        {/* <meta property="og:image" content="https://www.grovemarketingco.com/images/og-image.jpg" /> */}
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:url" content="https://www.grovemarketingco.com/" />
-        <meta
-          name="twitter:title"
-          content="Grove Marketing Co. | Digital Marketing, Web Development & SEO"
-        />
-        <meta
-          name="twitter:description"
-          content="Grove Marketing Co. helps bold brands grow with cutting-edge marketing, custom websites, content creation, and automation. Let’s make waves together."
-        />
-        {/* <meta name="twitter:image" content="https://www.grovemarketingco.com/images/og-image.jpg" /> */}
-
         <link rel="icon" href="/img/favicon.png" />
       </Head>
       <body className="bg-grove-600 text-grove-100 font-header w-full antialiased">
